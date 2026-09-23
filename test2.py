96e79b2303e8dfc6e57aca26ecac896f7335f23d
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-09-22 23:09:29 -0500

    Two walker simulation

--- test2.py
+++ test2.py
@@ -1,62 +1,78 @@
-import random 
+# two walkers
+import random
 
-def view_simulate(graph):
-    count = 0
-    current = 3
-    visited = {current}
-    walk = []
+graph1 = {
+    1: [2],
+    2: [1, 3],
+    3: [2, 4],
+    4: [3, 5],
+    5: [4]
+}
 
-    while len(visited) < len(graph):
-        walk.append(current)
-        neighbors = graph[current]
+graph2 = {
+    1: [2],
+    2: [1, 3],
+    3: [2, 4],
+    4: [3, 5],
+    5: [4, 6],
+    6: [5, 7],
+    7: [6, 8],
+    8: [7, 9],
+    9: [8, 10],
+    10: [9]
+}
 
-        print("Current:", current)
-        print("Neighbors:", neighbors)
+count = 0
 
-        next_vertex = random.choice(neighbors)
-        count+=1
-        current = next_vertex
-        visited.add(current)
+curr_A = 1
+curr_B = 5
 
-        print("Moved to:", current)
-        print("Visited:", visited)
-        print()
-    walk.append(current)
+visited_A = {curr_A}
+visited_B = {curr_B}
 
-    print(f"Number of moves: {count}")
-    print(f"Walk: {walk}")
-    print("Finished!")
-
-def simulate(graph):
-    count = 0
-    current = 3
-    visited = {current}
-
-    while len(visited) < len(graph):
-        neighbors = graph[current]
-
-        next_vertex = random.choice(neighbors)
-        count+=1
-        current = next_vertex
-        visited.add(current)
-
-    return count
-
-def main():
-    graph = { #straight line/walk
-        1: [2],
-        2: [1, 3],
-        3: [2, 4],
-        4: [3, 5],
-        5: [4]
-    }
-    number_of_simulations = 10
-    steps_needed = [] #sotres it
-    for i in range(number_of_simulations):
-        steps = simulate(graph)
-        steps_needed.append(steps)
-        print(f"Simulation {i+1} needed {steps} steps")
+walk_A = [curr_A]
+walk_B = [curr_B]
 
 
-if __name__ == '__main__':
-    main()
+while (len(visited_A) < len(graph1)
+       and len(visited_B) < len(graph1)
+       and curr_A != curr_B):
+    prob = random.random()
+
+    # A moves
+    if prob < 1/3:
+        neighbors = graph1[curr_A]
+        curr_A = random.choice(neighbors)
+        visited_A.add(curr_A)
+        walk_A.append(curr_A)
+
+    # B moves
+    elif prob < 2/3:
+        neighbors = graph1[curr_B]
+        curr_B = random.choice(neighbors)
+        visited_B.add(curr_B)
+        walk_B.append(curr_B)
+
+    # Neither moves
+    else:
+        walk_A.append(curr_A)
+        walk_B.append(curr_B)
+
+    count += 1
+
+
+print("Finished!")
+print("Number of moves:", count)
+print("A's walk:", walk_A)
+print("B's walk:", walk_B)
+print("A visited:", visited_A)
+print("B visited:", visited_B)
+
+if curr_A == curr_B:
+    print("A and B touched each other!")
+
+elif len(visited_A) == len(graph1):
+    print("A visited every vertex!")
+
+elif len(visited_B) == len(graph1):
+    print("B visited every vertex!")
